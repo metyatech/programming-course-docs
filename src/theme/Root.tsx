12d49ad2283f705b020d5db94d9c6a99d28ef135
Kodai-Yamamoto-SIW <k-yamamoto@siw.ac.jp>
2025-09-30 09:39:29 +0900

    不要な Root を削除し、package-lock を再生成した

--- src/theme/Root.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '@kodai-yamamoto-siw/code-preview/styles.css';
-
-export default function Root({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-}
